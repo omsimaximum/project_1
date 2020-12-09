81bef207857b86c0c6020b8a214b3d22128263e9
joshkile <joshkilegementiza@gmail.com>
2020-12-09 13:24:57 +0800

    Upload content

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 let hamburger = document.querySelector('#hamburger');
 let close = document.querySelector('#close');
 let slide = document.querySelector('.slide');
+const counters = document.querySelectorAll('.counter')
 
 
 
@@ -10,10 +11,37 @@ close.addEventListener('click',closeSideMenu);
 
 
 function sideMenu(e){
-	slide.style.right = '0px';
-	
+	slide.style.right = '0px';	
 }
 
 function closeSideMenu(e){
 	slide.style.right = '-300px';
+}
+
+
+window.onscroll = function count (){
+
+if(window.window.scrollY > 100);
+counters.forEach(counter => {
+
+	if (counter.innerText === '') {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        const c = +counter.innerText
+
+        const increment = target / 350;
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else {
+            counter.innerText = target
+        }
+    }
+
+    updateCounter()
+	}
+})
 }
